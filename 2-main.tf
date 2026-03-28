8ed4209dad93520ad91056303b93b0b697d736c6
Dave M <230270559+davem500@users.noreply.github.com>
2026-03-28 02:53:08 -0400

    third commit

--- 2-main.tf
+++ 2-main.tf
@@ -8,25 +8,25 @@ resource "aws_s3_bucket" "frontend" {
   }
 }
 
-resource "aws_s3_object" "picture" {
+resource "aws_s3_object" "arm_proof_image" {
   bucket = aws_s3_bucket.frontend.id
   key    = "deliverables/arm_proof_image.png"
   source = "${path.module}/deliverables/arm_proof_image.png"
 }
 
-resource "aws_s3_object" "text" {
+resource "aws_s3_object" "armageddon_project_link" {
   bucket = aws_s3_bucket.frontend.id
   key    = "deliverables/armageddon_project_link.txt"
   source = "${path.module}/deliverables/armageddon_project_link.txt"
 }
 
-resource "aws_s3_object" "picture" {
+resource "aws_s3_object" "pipeline-deployment" {
   bucket = aws_s3_bucket.frontend.id
   key    = "deliverables/pipeline-deployment.png"
   source = "${path.module}/deliverables/pipeline-deployment.png"
 }
 
-resource "aws_s3_object" "picture" {
+resource "aws_s3_object" "web-hook-delivery" {
   bucket = aws_s3_bucket.frontend.id
   key    = "deliverables/arm_proof_image.png"
   source = "${path.module}/deliverables/web-hook-delivery.png"
