52f510bdd7a33d714c6e29ae286deb9082ea0cbf
Dave M <230270559+davem500@users.noreply.github.com>
2026-03-28 03:20:58 -0400

    fifth commit

--- 2-main.tf
+++ 2-main.tf
@@ -20,14 +20,26 @@ resource "aws_s3_object" "armageddon_project_link" {
   source = "${path.module}/deliverables/armageddon_project_link.txt"
 }
 
-resource "aws_s3_object" "pipeline-deployment" {
+resource "aws_s3_object" "pipeline_deployment" {
   bucket = aws_s3_bucket.frontend.id
   key    = "deliverables/pipeline_deployment.png"
   source = "${path.module}/deliverables/pipeline_deployment.png"
 }
 
-resource "aws_s3_object" "web-hook-delivery" {
+resource "aws_s3_object" "pipeline_deployment_2" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "deliverables/pipeline_deployment_2.png"
+  source = "${path.module}/deliverables/pipeline_deployment_2.png"
+}
+
+resource "aws_s3_object" "web_hook_delivery" {
   bucket = aws_s3_bucket.frontend.id
   key    = "deliverables/web_hook_delivery.png"
   source = "${path.module}/deliverables/web_hook_delivery.png"
+}
+
+resource "aws_s3_object" "s3_bucket_evidence" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "deliverables/s3_bucket_evidence.png"
+  source = "${path.module}/deliverables/s3_bucket_evidence.png"
 }
